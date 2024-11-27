817043a58582b0d7013527f84726a63e29f23459
Cyrill Semah <cyrillsemah@gmail.com>
2024-11-28 00:25:00 +0100

    feat: ajout de la prévisualisation des documents
    - Déplacement de la colonne preview à droite
    - Ajout d'une modal pour agrandir les images et PDFs
    - Ajout des méthodes de prévisualisation dans le modèle Document

--- app/models/document.rb
+++ app/models/document.rb
@@ -6,4 +6,35 @@ class Document < ApplicationRecord
   validates :cloudinary_id, presence: true, if: -> { file.present? }
 
   attr_accessor :file
+
+  def image?
+    return false unless cloudinary_id.present?
+    %w[jpg jpeg gif png webp].include?(format)
+  end
+
+  def pdf?
+    return false unless cloudinary_id.present?
+    format == 'pdf'
+  end
+
+  def previewable?
+    image? || pdf?
+  end
+
+  def format
+    return nil unless cloudinary_id.present?
+    Cloudinary::Api.resource(cloudinary_id)['format']
+  rescue => e
+    Rails.logger.error "Cloudinary error: #{e.message}"
+    nil
+  end
+
+  def preview_url
+    return nil unless cloudinary_id.present? && previewable?
+    if image?
+      Cloudinary::Utils.cloudinary_url(cloudinary_id, width: 200, crop: :fit)
+    else # pdf
+      Cloudinary::Utils.cloudinary_url(cloudinary_id, format: 'jpg', width: 200, crop: :fit, page: 1)
+    end
+  end
 end
