78f62aa00564e98e403af2bf0a4d47afb95a0a02
Maxime Franc <maxime.franc1@gmail.com>
2024-11-29 11:49:05 +0100

    fix messages, AND notes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_28_135615) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_29_100957) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_28_135615) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["conversation_id", "user_id"], name: "index_conversation_users_on_conversation_id_and_user_id", unique: true
     t.index ["conversation_id"], name: "index_conversation_users_on_conversation_id"
     t.index ["user_id"], name: "index_conversation_users_on_user_id"
   end
