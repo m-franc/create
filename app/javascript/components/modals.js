81a17d0460645b3efaab93cf583a7720f0f11dfd
Cyrill Semah <cyrillsemah@gmail.com>
2024-12-04 16:15:50 +0100

    modal apparission fixed

--- app/javascript/components/modals.js
+++ app/javascript/components/modals.js
@@ -1,4 +1,4 @@
-document.addEventListener("shown.bs.modal", function (event) {
+document.addEventListener("show.bs.modal", function (event) {
   const modal = event.target;
   document.body.append(modal);
 });
